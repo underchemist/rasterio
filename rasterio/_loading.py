56d9e0eb5a2acfc3541bad8d0a4700014fb57558
Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2021-04-07 12:47:36 -0700

    fix dll load

--- rasterio/_loading.py
+++ rasterio/_loading.py
@@ -16,10 +16,16 @@ log.addHandler(logging.NullHandler())
 @contextlib.contextmanager
 def add_gdal_dll_directories():
     dll_dirs = []
-    if platform.system() == 'Windows' and sys.version_info >= (3, 8):
-        dll_directory = os.path.join(os.path.dirname(__file__), '.libs')
-        if os.path.exists(dll_directory):
-            dll_dirs.append(os.add_dll_directory(dll_directory))
+    dll_directory = os.path.dirname(__file__) + '.libs'
+    if platform.system() == 'Windows' and os.path.isdir(dll_directory):
+        if sys.version_info >= (3, 8):
+            if os.path.exists(dll_directory):
+                dll_dirs.append(os.add_dll_directory(dll_directory))
+            else:
+                if 'PATH' in os.environ:
+                    for p in os.environ['PATH'].split(os.pathsep):
+                        if glob.glob(os.path.join(p, 'gdal*.dll')):
+                            os.add_dll_directory(p)
         else:
             if 'PATH' in os.environ:
                 for p in os.environ['PATH'].split(os.pathsep):
